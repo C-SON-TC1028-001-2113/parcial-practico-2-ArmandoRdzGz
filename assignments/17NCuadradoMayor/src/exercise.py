7843f6b83199d5b3abdbcc236aabc96ca4fc79bb
ArmandoRdzGz <A01254249@itesm.mx>
2021-09-10 17:55:14 +0000

    Examen_practica2

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,11 @@
 
-
+import math
 def main():
-    numero = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-
-
+    numero = int(input("Escribe un numero : "))
+    v = 1
+    while numero>= v**2:
+        v = v+1
+    print (v)
 if __name__=='__main__':
     main()
